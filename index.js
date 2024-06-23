7d8a1d024409fdc3c9453ef8319602235cbc9b99
Nick Pearson <Nick-Pearson@users.noreply.github.com>
2024-06-23 19:39:43 +0100

    display output in a div

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import { explain } from './pkg';
 
-console.log(explain('Race Car'));
+// add event on document ready
+function init() {
+    console.log('init');
+
+    const mainInput = document.getElementById('mainInput')
+    const mainOutput = document.getElementById('mainOutput')
+    const updateExplanation = function() {
+        mainOutput.innerHTML = explain(mainInput.value)
+    }
+
+    mainInput.addEventListener('keydown', updateExplanation)
+    mainInput.addEventListener('keyup', updateExplanation)
+    updateExplanation()
+}
+
+init();
